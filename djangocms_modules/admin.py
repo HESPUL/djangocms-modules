b8bff687b7ed026e3442649821e0caa6473650e5
Alexandre Norman <norman@xael.org>
2025-04-17 16:11:45 +0200

    fix: remove deprecated PlaceholderAdminMixin from CategoryAdmin

--- djangocms_modules/admin.py
+++ djangocms_modules/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 
-from cms.admin.placeholderadmin import PlaceholderAdminMixin
-
 from .models import Category
 
 
 @admin.register(Category)
-class CategoryAdmin(PlaceholderAdminMixin, admin.ModelAdmin):
-    list_display = ['name']
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
